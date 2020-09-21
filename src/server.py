857b8d722d6dbd1ba39be9dc046cc47883f773a2
Dhananjay Shukla <dhananjay.shukla7991@gmail.com>
2020-09-22 03:48:52 +0530

    Modified login route

--- src/server.py
+++ src/server.py
@@ -1,14 +1,16 @@
 #Basic flask app
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'This is a home page'
 
+
 @app.route('/login')
-def login():
-    return (5+9);
+def login(name=None):
+    return render_template('index.html', name=name)
 
 @app.route('/signup')
 def signup():
