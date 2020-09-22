e13de113175dd5da95850134bdce33ead27e2666
Dhananjay Shukla <dhananjay.shukla7991@gmail.com>
2020-09-22 17:23:38 +0530

    Added login route

--- server.py
+++ server.py
@@ -1,16 +1,34 @@
 #Basic flask app
-from flask import Flask
-from flask import render_template
+from flask import Flask,render_template,request
+from flask_pymongo import PyMongo
+from cfg import config
+import json
+#Flask App Configuration
 app = Flask(__name__)
+app.config["MONGO_URI"] = config['mongo_URI']
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def home():
-    return 'This is a home page'
+    #users is our collection name
+    user_docs = mongo.db.users.find({})
+    user_docs=list(user_docs)
+    for doc in user_docs:
+        print(doc)
+    return 'This is home page'
 
 
 @app.route('/login')
 def show_login():
-    return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/check_login',methods=['GET','POST'])
+def check_login():
+    email=request.form['email']
+    password=request.form['password']
+    return "Email is"+" "+email+'\n'+"Password is"+" "+password
+
 
 @app.route('/signup')
 def signup():
